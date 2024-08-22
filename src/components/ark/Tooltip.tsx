49465c6f943245d7605b6df84d9acb821c31be0c
uhuikim <53680212+uhuikim@users.noreply.github.com>
2024-08-22 10:24:59 +0900

    수정

--- src/components/ark/Tooltip.tsx
+++ src/components/ark/Tooltip.tsx
@@ -1,16 +1,19 @@
 import { css } from "@/styled-system/css";
 import { Tooltip } from "@ark-ui/react";
+
 const TooltipDemo = () => {
   return (
     <Tooltip.Root>
       <Tooltip.Trigger>Hover Me</Tooltip.Trigger>
-      <Tooltip.Positioner
-        className={css({
-          background: "yellow",
-          padding: 10,
-        })}
-      >
-        <Tooltip.Content>I am a tooltip!</Tooltip.Content>
+      <Tooltip.Positioner>
+        <Tooltip.Content
+          className={css({
+            background: "yellow",
+            padding: 10,
+          })}
+        >
+          I am a tooltip!
+        </Tooltip.Content>
       </Tooltip.Positioner>
     </Tooltip.Root>
   );
